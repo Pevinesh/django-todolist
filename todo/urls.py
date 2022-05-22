cac3b37e96094158c6ba1dc5841adf6f00108e46
pevinesh B <pevinesh2001@gmail.com>
2022-05-22 22:43:20 +0530

    first commit

---
+++ todo/urls.py
@@ -0,0 +1,9 @@
+from ast import Delete
+from django import views
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.index,name='index'),
+    path('delete/<str:pk>',views.delete,name='delete')
+]
